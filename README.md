74b063ae67cd29987af7301eac024906bcfd6d9f
Clay <bigafro3@gmail.com>
2019-09-19 01:22:36 -0400

    add game display function as well as update README and link js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # game_show_app
+
+This is a browser version of “Wheel of Success”, a word guessing game where players will click letters from an onscreen keyboard to try to guess a random phrase. 
---
+++ js/main.js
@@ -0,0 +1,40 @@
+const qwerty = document.getElementById('qwerty');
+const phraseId = document.getElementById('phrase');
+let missed = 0;
+const phrases = ['what you talkin bout willis', 'born to run', 'imageine', 'let it be', 'yesterday'];
+const overlay = document.getElementById('overlay');
+const btnReset = document.querySelector('.btn__reset');
+
+btnReset.addEventListener('click', () => {
+  overlay.style.display = 'none';
+})
+
+function getRandomPhraseAsArray(arr) {
+    const randomPhrase = phrases[(parseInt(Math.random() * phrases.length))];
+    const splitRandomPhrase = randomPhrase.split('');
+    return splitRandomPhrase;
+}
+//console.log(getRandomPhraseAsArray(phrases));
+
+//Game display
+function addPhraseToDisplay() {
+    //let userInput = [ ];
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    for(let i = 0; i < phraseArray.length; i++) {
+        const li = document.createElement('li');
+        const ul = document.querySelector('ul');
+        ul.appendChild(li);
+        li.appendChild(phraseArray);
+        if (phraseArray == ' ') {
+            return null;
+        } else {
+             li.className = 'letter';
+        }
+    }
+    console.log(phraseArray); 
+}
+addPhraseToDisplay();
+//all characters have the class letters applied, other than the appendChild.
+
+
+
